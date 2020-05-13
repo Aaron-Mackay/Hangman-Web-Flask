2e3fd102bb929602caf405a4aad045b2811e9634
Aaron Mackay <aaronmackay@hotmail.com>
2020-05-13 21:00:19 +0100

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hangman Companion Website
+
+The web companion  to the android app, hosted from a Raspberry Pi, accessable at www.playhangman.co.uk
+
+## Goals
+* **List upload** - Users can upload a list of phrases, which are then saved to the server as a csv, for access and use from the app.
+* **Scoreboards** - As in the app, allow access to scoreboards on different lists
+* **Gameplay** (long-term) - Allow gameplay through the website
---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, send_file, abort
+import csv
+from glob import glob
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    fileList = getFileList()
+    return render_template("index.html")
+
+
+@app.route('/api/<phraseList>.csv')
+def csv_test(phraseList):
+    try:
+        return send_file(f"listFiles/{phraseList}.csv",
+                         mimetype='text/csv',
+                         attachment_filename=f"{phraseList}.csv",
+                         as_attachment=True)
+    except OSError:
+        abort(404)
+
+
+@app.route('/save', methods=['GET', 'POST'])
+def save():
+    if request.method == "POST":
+        #todo if filename contains _, if filename used
+        listName = request.form.get("listName").replace(" ", "_")
+        list = request.form.get("phrases").split("\r\n")
+        print(list, listName)
+        with open(f"listFiles/{listName}.csv", 'w+', newline='') as listFile:
+            wr = csv.writer(listFile)
+            for i in list:
+                wr.writerow([i])
+
+
+    else:
+        return redirect("/")
+
+
+def getFileList():
+    csvDir = 'listFiles'
+    filelist = glob(f'{csvDir}/*.csv')
+    namedFileList = []
+    for i in filelist:
+        string = i.replace(f"{csvDir}\\", "").replace("_", " ").replace(".csv", "").title()
+
+        namedFileList.append(string)
+    print(filelist)
+    print(namedFileList)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
